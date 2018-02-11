831c9c8c57639a429bca7adecf0ebbb217bdd755
Paige Pittman <paigepittman@mac.com>
2018-02-11 13:12:29 -0800

    product component displaying properly

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import products from '../product-payload.json';
+
 
 class Shop extends Component {
+
+  state = {products: products.products}
+
   render() {
     return(
-      <div>
-        Shop
-      </div>
+    <div class="container">
+      <div class="row products-view">
+        {this.state.products.map((product,i) =>
+            <div class="col s12 m3">
+              <div class="card">
+                <div class="card-image">
+                  <img src={"./images/" + product.filename} />
+                  <span class="card-title"></span>
+                </div>
+                <div class="card-content">
+                  <p id="name">{product.name}</p>
+
+                <p id="price">  ${product.price} </p>
+
+                  <a class="add-button" href="#">Add to cart</a>
+                </div>
+              </div>
+            </div>
+
+                )}
+          </div>
+        </div>
     )
   }
 }
